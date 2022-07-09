17445feb79fccef132575abd4bcef2be704ea4ba
Naerdel <eqillibrium@live.ru>
2022-07-09 16:53:06 +0300

    added some components

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+// eslint-disable-next-line no-undef
+module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      loader: '@svgr/webpack',
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [{
+            name: 'preset-default',
+            params: {
+              override: {
+                removeViewBox: false
+              }
+            }
+          }]
+        },
+        titleProp: true
+      },
+      test: /\.svg$/
+    });
+    return config;
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from 'next';
+import { Button } from '../components'
 
 const Home: NextPage = () => {
   return (
     <main>
-
+      <Button appearance='primary' arrow='right'>Кнопка</Button>
     </main>
   );
 };
